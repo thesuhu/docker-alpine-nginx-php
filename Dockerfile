d34025a27dd73b53034fbe4ee92207492de4adcf
The Suhu <thesuhu@protonmail.com>
2023-06-26 13:16:03 +0700

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM 8.2.7-fpm-alpine3.18
+
+RUN apk update \
+    && apk add --no-cache nginx supervisor git curl
+
+# config
+COPY ./config/nginx.conf /etc/nginx/nginx.conf
+COPY ./config/supervisord.conf /etc/supervisord.conf
+
+# default page
+COPY ./config/index.php /var/www/html/index.php
+COPY ./config/phpinfo.php /var/www/html/phpinfo.php
+
+# scripts
+COPY ./scripts/configure.sh /configure.sh
+
+EXPOSE 80/tcp
+
+RUN chmod +x /configure.sh && sh /configure.sh
+
+CMD ["/usr/bin/supervisord", "-c", "/etc/supervisord.conf"]
---
+++ scripts/configure.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Nginx config
+adduser -D -g 'www-data' www-data
+chown -R www-data:www-data /var/lib/nginx
+mkdir /run/nginx && touch /run/nginx/nginx.pid
+
+# Set variables
+PHP_FPM_USER="www-data"
+PHP_FPM_GROUP="www-data"
+PHP_FPM_LISTEN_MODE="0660"
+PHP_MEMORY_LIMIT="512M"
+PHP_MAX_UPLOAD="2M"
+PHP_MAX_FILE_UPLOAD="20"
+PHP_MAX_POST="8M"
+PHP_DISPLAY_ERRORS="Off"
+PHP_DISPLAY_STARTUP_ERRORS="Off"
+PHP_ERROR_REPORTING="E_ALL & ~E_DEPRECATED & ~E_STRICT"
+PHP_CGI_FIX_PATHINFO=0
+
+# Replace some of the configurations in www.conf
+sed -i "s|;listen.owner\s*=\s*nobody|listen.owner = ${PHP_FPM_USER}|g" /etc/php8/php-fpm.d/www.conf
+sed -i "s|;listen.group\s*=\s*nobody|listen.group = ${PHP_FPM_GROUP}|g" /etc/php8/php-fpm.d/www.conf
+sed -i "s|;listen.mode\s*=\s*0660|listen.mode = ${PHP_FPM_LISTEN_MODE}|g" /etc/php8/php-fpm.d/www.conf
+sed -i "s|user\s*=\s*nobody|user = ${PHP_FPM_USER}|g" /etc/php8/php-fpm.d/www.conf
+sed -i "s|group\s*=\s*nobody|group = ${PHP_FPM_GROUP}|g" /etc/php8/php-fpm.d/www.conf
+sed -i "s|;log_level\s*=\s*notice|log_level = notice|g" /etc/php8/php-fpm.d/www.conf
+
+# Replace some of the configurations in php.ini
+sed -i "s|display_errors\s*=\s*Off|display_errors = ${PHP_DISPLAY_ERRORS}|i" /etc/php8/php.ini
+sed -i "s|display_startup_errors\s*=\s*Off|display_startup_errors = ${PHP_DISPLAY_STARTUP_ERRORS}|i" /etc/php8/php.ini
+sed -i "s|error_reporting\s*=\s*E_ALL & ~E_DEPRECATED & ~E_STRICT|error_reporting = ${PHP_ERROR_REPORTING}|i" /etc/php8/php.ini
+sed -i "s|;*memory_limit =.*|memory_limit = ${PHP_MEMORY_LIMIT}|i" /etc/php8/php.ini
+sed -i "s|;*upload_max_filesize =.*|upload_max_filesize = ${PHP_MAX_UPLOAD}|i" /etc/php8/php.ini
+sed -i "s|;*max_file_uploads =.*|max_file_uploads = ${PHP_MAX_FILE_UPLOAD}|i" /etc/php8/php.ini
+sed -i "s|;*post_max_size =.*|post_max_size = ${PHP_MAX_POST}|i" /etc/php8/php.ini
+sed -i "s|;*cgi.fix_pathinfo=.*|cgi.fix_pathinfo= ${PHP_CGI_FIX_PATHINFO}|i" /etc/php8/php.ini
